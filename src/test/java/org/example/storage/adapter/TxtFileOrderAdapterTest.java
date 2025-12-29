bf6c284f64341c7ee4ff773512ceb31073a3952a
Azaliya <ziganshina.azaliya1@gmail.com>
2025-12-29 20:28:46 +0300

    Решение 9

--- src/test/java/org/example/storage/adapter/TxtFileOrderAdapterTest.java
+++ src/test/java/org/example/storage/adapter/TxtFileOrderAdapterTest.java
@@ -2,9 +2,14 @@ package org.example.storage.adapter;
 
 import org.example.exception.IORuntimeException;
 import org.example.model.Order;
+import org.example.model.OrderReport;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,7 +20,7 @@ class TxtFileOrderAdapterTest {
     @Test
     void readTxtFileOrderTest() {
         Order order = new Order(LocalDateTime.of(2021, 2, 9, 16, 0, 22), "Industrial", 8800);
-        String path = "C:\\Users\\Admin\\IdeaProjects\\Java2\\src\\main\\java\\org\\example\\files\\discount_day.txt";
+        String path = "C:\\Users\\Admin\\IdeaProjects\\Java2\\src\\test\\java\\org\\example\\testFiles\\discount_day.txt";
         Order orderTest = file.read(path).get(0);
         assertEquals(order.getName(), orderTest.getName());
         assertEquals(order.getTime(), orderTest.getTime());
@@ -24,14 +29,23 @@ class TxtFileOrderAdapterTest {
 
     @Test
     void readEmptyFileReturnsEmptyList() {
-        String path = "C:\\Users\\Admin\\IdeaProjects\\Java2\\src\\main\\java\\org\\example\\files\\result.txt";
+        String path = "C:\\Users\\Admin\\IdeaProjects\\Java2\\src\\test\\java\\org\\example\\testFiles\\result.txt";
         List<Order> orders = file.read(path);
         assertTrue(orders.isEmpty(), "Список пустой, как и файл");
     }
 
     @Test
     void readFileWithIncorrectContentThrowsException() {
-        String path = "C:\\Users\\Admin\\IdeaProjects\\Java2\\src\\main\\java\\org\\example\\files\\exception.txt";
+        String path = "C:\\Users\\Admin\\IdeaProjects\\Java2\\src\\test\\java\\org\\example\\testFiles\\etion.txt";
         assertThrows(IORuntimeException.class, () -> file.read(path));
+    }
+
+    @Test
+    void writeTxtFileOrderTest() throws IOException {
+        String path = "C:\\Users\\Admin\\IdeaProjects\\Java2\\src\\test\\java\\org\\example\\testFiles\\resultWrite.txt";
+        List<OrderReport> reports = List.of(new OrderReport("Industrial", 79200.0));
+        file.writeFile(path, reports);
+        List<String> reportTest = Files.readAllLines(Path.of(path));
+        assertEquals("Industrial - 79200.0", reportTest.get(0));
     }
 }
